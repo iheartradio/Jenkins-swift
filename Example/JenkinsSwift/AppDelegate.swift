5ba1cc878d66bc495ce2ad6dc0d4dec5243023c1
AdibContractorCC <adibcontractor@iheartmedia.com>
2018-12-17 23:37:30 -0500

    Cocoapod

---
+++ Example/JenkinsSwift/AppDelegate.swift
@@ -0,0 +1,26 @@
+//
+//  AppDelegate.swift
+//  JenkinsSwift
+//
+//  Created by adibcontractorcc on 12/17/2018.
+//  Copyright (c) 2018 adibcontractorcc. All rights reserved.
+//
+
+import Cocoa
+
+@NSApplicationMain
+class AppDelegate: NSObject, NSApplicationDelegate {
+
+
+
+  func applicationDidFinishLaunching(_ aNotification: Notification) {
+    // Insert code here to initialize your application
+  }
+
+  func applicationWillTerminate(_ aNotification: Notification) {
+    // Insert code here to tear down your application
+  }
+
+
+}
+
---
+++ Example/JenkinsSwift/ViewController.swift
@@ -0,0 +1,27 @@
+//
+//  ViewController.swift
+//  JenkinsSwift
+//
+//  Created by adibcontractorcc on 12/17/2018.
+//  Copyright (c) 2018 adibcontractorcc. All rights reserved.
+//
+
+import Cocoa
+
+class ViewController: NSViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+
+    // Do any additional setup after loading the view.
+  }
+
+  override var representedObject: Any? {
+    didSet {
+    // Update the view, if already loaded.
+    }
+  }
+
+
+}
+
